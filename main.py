04413aa5c838582949a94ce70d1fead74a655233
meamaturinlove221 <83173275+meamaturinlove221@users.noreply.github.com>
2023-05-08 11:38:55 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -105,10 +105,17 @@ def app():
             key_names = input_2d_file.files
             # 使用方括号来索引对应的数据，假设你想加载'model_pos'这个键的数据，它的形状是(N, J, 2)
             input_2d = input_2d_file['model_pos']
+            # 使用方括号来索引对应的数据，假设你想加载'model_pos'这个键的数据，它的形状是(N, J, 2)
+            input_2d = input_2d_file['model_pos']
             # 使用model_pos.load_state_dict函数来加载模型的参数
-            model_pos.load_state_dict(
-                torch.load('checkpoint/pretrained_h36m_detectron_coco.bin', map_location='cpu')['model_pos'])
-            #model_pos = TemporalModel(17, 2, 17, filter_widths=[3, 3], causal=False, dropout=0.5, channels=1024)
+            state_dict = torch.load('checkpoint/pretrained_h36m_detectron_coco.bin', map_location='cpu')['model_pos']
+            # 如果模型和state_dict的键不完全一致，可以使用strict=False参数来忽略不匹配的部分
+            model_pos.load_state_dict(state_dict, strict=False)
+            # 或者使用missing_keys和unexpected_keys参数来获取缺失或多余的参数列表
+            missing_keys, unexpected_keys = model_pos.load_state_dict(state_dict)
+            # 打印缺失或多余的参数列表
+            print("Missing keys:", missing_keys)
+            print("Unexpected keys:", unexpected_keys)
             # 创建一个空的数组来存储图像中的2D关键点数据，它的形状是(N, J, 2)
             image_2d = np.zeros((input_2d.shape[0], input_2d.shape[1], 2))
             # 对每一帧的2D关键点数据进行处理，将其转换为像素坐标，并存储到输出数组中
